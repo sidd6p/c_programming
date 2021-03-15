36746913314bb43efe48e5c2c2849b5383a51980
siddhartha purwar <siddpurwar6@gmail.com>
2021-03-15 19:54:45 +0530

    removing unnecessary files

---
+++ OS/a_2/q2.c
@@ -0,0 +1,95 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#define MAXCHARACTER 256
+
+int anagram_check();
+int isomorphic_check();
+
+int anagram_check(){
+  int c, ncharcte1[MAXCHARACTER], ncharcte2[MAXCHARACTER], i;
+  i = 0;
+  for(i; i < MAXCHARACTER; i++){
+    ncharcte1[i] = ncharcte2[i] = 0;
+  }
+    printf("Enter first string");
+    fflush(stdin);
+    while((c = getchar()) != '\n'){
+      ncharcte1[c - '0']++;
+    }
+    printf("Enter second string\n");
+    fflush(stdin);
+    while((c = getchar()) != '\n'){
+      ncharcte2[c - '0']++;
+    }
+    for(i = 0; i < MAXCHARACTER; i++){
+      if(ncharcte1[i] != ncharcte2[i]){
+        return 0;
+      }
+    }
+    return 1;
+}
+
+int isomorphic_check(){
+  int c, ncharcte1[MAXCHARACTER], ncharcte2[MAXCHARACTER], i;
+  i = 0;
+  for(i; i < MAXCHARACTER; i++){
+    ncharcte1[i] = ncharcte2[i] = 0;
+  }
+    printf("Enter first string");
+    fflush(stdin);
+    while((c = getchar()) != '\n'){
+      ncharcte1[c - '0']++;
+    }
+    printf("Enter second string\n");
+    fflush(stdin);
+    while((c = getchar()) != '\n'){
+      ncharcte2[c - '0']++;
+    }
+    for(i = 0; i < MAXCHARACTER; i++){
+      for(int j = 0; i < MAXCHARACTER; j++){
+        if(ncharcte1[i] == ncharcte2[j]){
+          ncharcte1[i] = ncharcte2[j] = 0;
+          break;
+        }
+      }
+    }
+    for(i = 0; i < MAXCHARACTER; i++){
+      if((ncharcte1[i] != 0) || (ncharcte2[i] != 0)){
+        return 0;
+      }
+    }
+
+}
+
+
+int main()
+{
+pid_t  pid;
+        pid = fork();
+        if (pid < 0) {
+                fprintf(stderr, "Fork Failed");
+                return 1;
+        }
+        else if (pid == 0) {
+                printf("\nChild Process is executing\n");
+                if(isomorphic_check()){
+                  printf("Given string are isomorphic\n");
+                }
+                else{
+                  printf("Given string are not isomorphic\n");
+                }
+        }
+        else
+        {
+                printf("\nParent Process is executing\n");
+                if(anagram_check()){
+                  printf("Given string are anagram\n");
+                }
+                else{
+                  printf("Given string are not anagram\n");
+                }
+
+        }
+        return 0;
+}
