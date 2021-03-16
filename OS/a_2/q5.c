1f0e42245fc99f3dce1459345beb50b16ef9d511
siddhartha purwar <siddpurwar6@gmail.com>
2021-03-16 22:35:25 +0530

    done with pkill() in OS

--- OS/a_2/q5.c
+++ OS/a_2/q5.c
@@ -1,40 +1,23 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h>
-
+#include <stdlib.h>
+#include <string.h>
 
 int main()
 {
-pid_t  pid;int status;
-        pid = fork();
-        if (pid < 0) {
-                fprintf(stderr, "Fork Failed");
-                return 1;
-        }
-        else if (pid == 0) {
-                printf("\nChild Process is executing\n");
-                printf("Child Process ID is %d\n" , getpid());
-                printf("It's Parent Process ID is %d\n" , getppid());
-                printf("pid variable in child process is %d\n", pid);
-                printf("\n..........................\n\n");
-                sleep(1);//child will be sleeping for 1 seconds
-                printf("This line will npt get print, because parent will kill child before executing of this line\n");
-
-        }
-        else
-        {
-                printf("\nParent Process is executing\n");
-                printf("Parent Process ID is %d\n" , getpid());
-                printf("It's Parent Process ID is %d\n" , getppid());
-                printf("pid variable in parent process is %d\n", pid);
-                sleep(1);
-                kill(pid, SIGKILL);
-                return 0;
-        }
-        printf("parent");
-        kill("myName");
-
-
-        return 0;
+  char process_name[100];
+  char kill_command[] = "pkill ";
+  int status = 868;
+  printf("Enter the process name to kill\n");
+  scanf("%s", process_name);
+  strcat(kill_command, process_name);
+  status = system(kill_command);
+  if(!status){
+    printf("Process killed successfully\n");
+  }
+  else{
+    printf("Process killing unsuccessfully\n");
+  }
+  return 0;
 }
